eaf2f4967b7293ae4eea193ef580ddd56d10a0cc
AslamJM <jma8744@gmail.com>
2024-12-01 10:02:48 +0530

    init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"aoc-2024/utils"
+	"fmt"
+)
+
+func main() {
+	s := utils.ReadFile("test")
+	fmt.Println(utils.GetLines(s))
+}
---
+++ utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	fildir = "inputs/"
+)
+
+func ReadFile(fileName string) string {
+	input, err := os.ReadFile(fildir + fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(input)
+}
+
+func GetLines(s string) []string {
+	return strings.Split(s, "\r\n")
+}
