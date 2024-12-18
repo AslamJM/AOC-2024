8feac9f12d3338da049e1ef43ec93221d82f48bd
AslamJM-SPMPC <jma8744@gmail.com>
2024-12-18 12:54:37 +0530

    day6 part 1

---
+++ day6/day6.go
@@ -0,0 +1,122 @@
+package day6
+
+import (
+	"aoc-2024/utils"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+const fileName = "input"
+
+func parseInput() [][]string {
+	s := utils.ReadFile(fileName)
+	lines := utils.GetLines(s)
+
+	var out [][]string
+
+	for _, l := range lines {
+		pos := strings.Split(l, "")
+		out = append(out, pos)
+	}
+
+	return out
+}
+
+func findStart() (int, int) {
+	s := parseInput()
+
+	for i, l := range s {
+		for j, c := range l {
+			if c == "^" {
+				return j, i
+			}
+		}
+	}
+
+	return 0, 0
+}
+
+func isExit(dir string, h, w, x, y int) bool {
+	if dir == "^" || dir == "v" {
+		if h == y {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	if dir == ">" || dir == "<" {
+		if w == x {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	return false
+}
+
+func move(input *[][]string, x, y int) (int, int) {
+	inp := *input
+	dir := inp[y][x]
+
+	switch dir {
+	case "^":
+		if inp[y-1][x] == "#" {
+			inp[y][x] = ">"
+			return x, y
+		} else {
+			inp[y-1][x] = "^"
+			return x, y - 1
+		}
+	case ">":
+		if inp[y][x+1] == "#" {
+			inp[y][x] = "v"
+			return x, y
+		} else {
+			inp[y][x+1] = ">"
+			return x + 1, y
+		}
+	case "v":
+		if inp[y+1][x] == "#" {
+			inp[y][x] = "<"
+			return x, y
+		} else {
+			inp[y+1][x] = "v"
+			return x, y + 1
+		}
+	case "<":
+		if inp[y][x-1] == "#" {
+			inp[y][x] = "^"
+			return x, y
+		} else {
+			inp[y][x-1] = "<"
+			return x - 1, y
+		}
+	default:
+		return x, y
+
+	}
+}
+
+func Part1() {
+	input := parseInput()
+	x, y := findStart()
+	h, w := len(input)-1, len(input[0])-1
+
+	var visited [][2]int = [][2]int{
+		{x, y},
+	}
+
+	for !isExit(input[y][x], h, w, x, y) {
+		x, y = move(&input, x, y)
+
+		if !slices.Contains(visited, [2]int{x, y}) {
+			visited = append(visited, [2]int{x, y})
+		}
+	}
+
+	fmt.Println(len(visited))
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"aoc-2024/day5"
+	"aoc-2024/day6"
 )
 
 func main() {
-	day5.Part2()
+	day6.Part1()
 }
